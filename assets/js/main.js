a7068f236344ef916bc716b8b5ca39fe4cdf3da9
André Spindola <uniaodk@gmail.com>
2024-03-30 16:18:59 -0300

    Feature consume data profile.json create html dynamic by template

---
+++ assets/js/main.js
@@ -0,0 +1,79 @@
+const headerElement = document.getElementById("header");
+const languagesElement = document.getElementById("languages");
+const respositoriesElement = doocument.getElementById("repositories");
+const experiencesElement = document.getElementById("experiences");
+const hardSkillsElement = document.getElementById("hard-skills");
+const softSkillsElement = document.getElementById("soft-skills");
+
+(async () => {
+	const profile = await getProfile();
+	headerElement.innerHTML += buildHeaderTemplate(profile.header);
+	languagesElement.innerHTML += buildLanguagesTemplate(profile.languages);
+	respositoriesElement.innerHTML += buildRepositoriesTemplate(profile.repositories);
+	experiencesElement.innerHtml += buildExperiencesTemplate(profile.languages);
+	hardSkillsElement.innerHtml += buildHardSkillsTemplate(profile.hardSkills);
+	softSkillsElement.innerHtml += buildSoftSkillsTemplate(profile.softSkills);
+})();
+
+function buildHeaderTemplate(header) {
+	return `<img class="photo" src="${header.path_photo}" alt="Picture ${header.name}">
+	<h1 class="title">Hi,<br>I am ${header.name}</h1>
+	<div class="information">
+		<a href="${header.url_linkedin}" target="_blank" class="job">${header.job}</a>
+		<p class="location">${header.location}</p>
+		<a href="tel:${header.phone}" class="phone">${header.phone}</a>
+		<a href="mailto:${header.email}" class="email">${header.email}</a>
+	</div>`
+}
+
+function buildLanguagesTemplate(languages) {
+	if (!languages) return '';
+	return languages.reduce((languagesTemplate, language) => {
+		let template = `<li class="language">`;
+		template += language.path_icon 
+			? `<span class="level">${language.level}</span>`
+			: `<img src="${language.path_icon}" alt="${language.alt_icon}">`;
+		template += `<span>${language.name}</span>
+		</li>`;
+		return languagesTemplate + template;
+	}, "");
+}
+
+function buildRepositoriesTemplate(repositories) {
+	if (!repositories) return '';
+	return repositories.reduce((repositoriesTemplate, repository) => {
+		return repositoriesTemplate + `<li class="repository">
+		<div class="detail">
+			<span>${repository.name}</span>
+			<a href="${repository.url}" target="_blank">${repository.url}</a>
+		</div>
+	</li>`
+	}, '');
+}
+
+function buildExperiencesTemplate(experiences) {
+	if (!experiences) return '';
+	return experiences.reduce((experiencesTemplate, experience) => {
+		return experiencesTemplate + `<li class="experience">
+		<h3>${experience.job}</h3>
+		<p class="period">${experience.period}</p>
+		<p class="info">${experience.info}</p>
+	</li>`
+	}, '');
+}
+
+function buildHardSkillsTemplate(hardSkills) {
+	if (!hardSkills) return '';
+	return hardSkills.reduce((hardSkillsTemplate, hardSkill) => {
+		return hardSkillsTemplate + `<li class="hard-skill">
+		<img src="${hardSkill.path_icon}" alt="${hardSkill.name}">
+	</li>`
+	}, '');
+}
+
+function buildSoftSkillsTemplate(softSkills) {
+	if (!softSkills) return '';
+	return softSkills.reduce((softSkillsTemplate, softSkill) => {
+		return softSkillsTemplate + `<li class="soft-skill">${softSkill}</li>`
+	}, '');
+}
